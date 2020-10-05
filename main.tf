e1dca29040e2cffb6b9b20804b607bbc4bd91a10
KonstantinKiessling <55312189+KonstantinKiessling@users.noreply.github.com>
2020-10-05 10:19:48 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws"{
+    region = "eu-west-1"
+}
+
+terraform{
+    backend "s3" {
+        bucket = "terraform-state-for-jenkins"
+        key = "terraform.tfstate"
+        region = "eu-west-1"
+    }
+}
+
+resource "aws_instance" "test"{
+    instance_type = "t3.micro"
+    ami = "ami-0bb3fad3c0286ebd5 (64-bit x86)"
+    key_name = "AWSkey"
+}
